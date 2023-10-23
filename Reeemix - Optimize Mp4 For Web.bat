01ac437e77f95d862615ca359280ce546d60bb18
__ysfk <yusufkocyigit67@gmail.com>
2023-10-23 15:30:00 +0200

    compatibility for all video types not only mp4

--- Reeemix - Optimize Mp4 For Web.bat
+++ Reeemix - Optimize Mp4 For Web.bat
@@ -4,11 +4,11 @@ setlocal enabledelayedexpansion
 rem Display software name
 echo.
 echo.
-echo Reeemix - Optimize Mp4 For Web
+echo Reeemix - Optimize Video Files for the +Web
 echo.
 echo.
 
-set /p customize_vars=Do you want to customize the variables (Y/N)? 
+set /p customize_vars=Do you want to customize the input/output folders and output file's suffix (Y/N)? 
 echo.
 echo.
 
@@ -22,22 +22,23 @@ if /i "!customize_vars!"=="Y" (
 )
 
 :main
-for %%i in ("!input_folder!\*.mp4") do (
+for %%i in ("!input_folder!\*.*") do (
     set "input=%%i"
     set "filename=%%~ni"
+    set "extension=%%~xi"
 
     rem Create the output folder if it doesn't exist
     if not exist "!output_folder!" mkdir "!output_folder!"
     
     rem Run the ffmpeg command
-    ffmpeg -i "!input!" -map 0 -c copy -movflags +faststart "!output_folder!\!filename!!output_suffix!.mp4"
+    ffmpeg -i "!input!" -map 0 -c copy -movflags +faststart "!output_folder!\!filename!!output_suffix!!extension!"
 )
 
 echo.
 echo.
 echo.
 echo.
-echo Thank you for using Reeemix !
+echo Thank you for using Reeemix!
 echo.
 echo.
 echo.
